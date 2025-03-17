60af8b653a55f2c1f95ae0b073fd63413054d23f
kashishpatil1218 <patilkashish649@gmail.com>
2025-03-17 13:46:08 +0530

    a

--- lib/utils/video_list.dart
+++ lib/utils/video_list.dart
@@ -8,35 +8,59 @@ List VideoList = [
   },
   {
     'video' : 'asset/video/v2.mp4',
-    'username' : '@Aryan_Arora1',
+    'username' : '@Aryan_Arora',
     'comments' : 4020,
     'likes' : '2.4k',
   },
 
   {
     'video' : 'asset/video/v3.mp4',
-    'username' : '@Aryan_Aroraw2',
+    'username' : '@Dog_love.01',
     'comments' : 4030,
     'likes' : '2.5k',
   },
 
   {
     'video' : 'asset/video/v4.mp4',
-    'username' : '@Aryan_Arora3',
+    'username' : '@Pro_memes',
     'comments' : 4800,
     'likes' : '2.6k',
   },
 
   {
     'video' : 'asset/video/v5.mp4',
-    'username' : '@Aryan_Arora4',
+    'username' : '@RCBens',
     'comments' : 4050,
     'likes' : '2.7k',
   },
 
   {
     'video' : 'asset/video/v6.mp4',
-    'username' : '@Aryan_Arora5',
+    'username' : '@Viratians_0089',
+    'comments' : 4070,
+    'likes' : '2.0k',
+  },
+  {
+    'video' : 'asset/video/v7.mp4',
+    'username' : '@Viratians_0089',
+    'comments' : 4070,
+    'likes' : '2.0k',
+  },
+  {
+    'video' : 'asset/video/v8.mp4',
+    'username' : '@Viratians_0089',
+    'comments' : 4070,
+    'likes' : '2.0k',
+  },
+  {
+    'video' : 'asset/video/09.mp4',
+    'username' : '@Viratians_0089',
+    'comments' : 4070,
+    'likes' : '2.0k',
+  },
+  {
+    'video' : 'asset/video/v10.mp4',
+    'username' : '@Viratians_0089',
     'comments' : 4070,
     'likes' : '2.0k',
   },
